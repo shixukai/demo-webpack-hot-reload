4492c8f5621cda46caede22072467f127e873313
施旭凯 <shixukai@163.com>
2016-12-04 22:05:41 +0800

    Init React Root Page

---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from 'frontend/src/root';
+
+ReactDOM.render(<Root />, document.getElementById('container'))
---
+++ frontend/src/root.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Root extends React.Component {
+  static propTypes = {
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Root</div>
+    )
+  }
+}
+
+export default Root;
